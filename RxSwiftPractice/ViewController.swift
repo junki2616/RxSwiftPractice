1618d36e6431755d0f5539caeed003f26e0c2764
junki-nishio <njunki0616@gmail.com>
2020-04-16 23:31:04 +0900

    動作確認

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
+
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rxTextField: UITextField!
+    @IBOutlet weak var rxLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,25 +25,34 @@ class ViewController: UIViewController {
         let observable = Observable.of("A","AB","ABC","B")
         
         _ = observable.filter{ 2 <= $0.count}
+            
+            // AB,ABC
             .subscribe(onNext:{ print($0)})
         
+            //配列要素ごとに出力
+        _ = Observable.from([1,2,3,4,5,"a"])
+            .subscribe(onNext:{ print($0)})
         
         //subjectを使った処理
         let subject = PublishSubject<String>()
         
-        subject.subscribe(onNext: {
+        
+        //subscribeの処理
+        _ = subject.subscribe(onNext: {
             print("onNext:", $0)
         })
         
+        //ストリームに流す
         subject.onNext("A")
         subject.onNext("B")
         subject.onNext("C")
         
-        
-        
-        
-        
     }
     
+    
+    @IBAction func rxTextFieldEC(_ sender: Any) {
+        
+    }
+
 }
 
